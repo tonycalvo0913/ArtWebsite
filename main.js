8d170381a154a117149895a607bdd550a98de123
tonycalvo0913 <40044838+tonycalvo0913@users.noreply.github.com>
2020-08-10 18:11:55 -0700

    Added Photoshop Table

--- main.js
+++ main.js
@@ -93,3 +93,94 @@ if (id) {
 } else {
   getDataForList();
 }
+
+
+//Should get information from Photoshop Art table
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyB7EQxA9Lt2lvXE'}).base('appeG8kkwJsPViya8');
+
+base('Photoshop Art').find('reclrZ2RzoXNlHrjJ', function(err, record) {
+    if (err) { console.error(err); return; }
+    console.log('Retrieved', record.id);
+});
+
+var listView = function(id, workname, rating) {
+    return `<div class="col-sm-6">
+    <div class="card mb-4 box-shadow">
+          <h2><a href="?id=${id}">${workname}</a></h2>
+          <div class="d-flex justify-content-between align-items-center">
+          <small class="text-muted">${rating}</small>
+          </div>
+        </div>
+    </div>`;
+  }
+// Get and display the data for all items
+ var getDataForList = function() {  
+  // This is where we get the JSON data from Airtable!
+  $.getJSON( airtable_list_url, function( data ) {
+    //console.log(data.records);
+    var html = [];
+    html.push(`<div class="row">`);
+    // 2. Iterates over every record and uses the list template
+    $.each( data.records, function( index, val ) {
+     //console.log(val.fields)
+        var id = val.id;
+        var fields = val.fields;
+        var workname = fields["WorkName"];
+        var rating = fields["Rating"];
+        var itemHTML = listView(id, workname, rating);
+        html.push(itemHTML);
+    });
+    html.push(`</div>`);
+  // 3. Adds HTML for every item to our page
+  $(".list-view").append(html.join(""));
+});
+}
+
+// Template that generates HTML for one item in our detail view, given the parameters passed in
+var detailView = function(id, workname, picture, explanation, rating) {
+  return `<div class="col-sm-12">
+    <div class="card mb-4 box-shadow">
+      <img class="card-img-top" src="${picture}">
+      <div class="card-body">
+        <h2><a href="?id=${id}">${workname}</a></h2>
+        <div class="d-flex justify-content-between align-items-center">
+          <small class="text-muted">${explanation}</small>
+          <small class="text-muted">${rating}</small>
+        </div>
+        <hr />
+      </div>
+    </div>
+  </div>`;
+}
+
+// Get and display the data for one item based on on the ID
+var getDataForId = function(id) {
+  $.getJSON(`https://api.airtable.com/v0/appeG8kkwJsPViya8/Hand%20Drawings/${id}?api_key=keyB7EQxA9Lt2lvXE`, function( record ) {
+    //console.log(data);
+    var html = [];
+    html.push(`<div class="row">`);
+      //console.log(val)
+      var id = record.id;
+      var fields = record.fields;
+      var workname = fields["WorkName"];
+      var picture= fields["Picture"] ? fields["Picture"][0].url : '';
+      var explanation = fields["Explanation"];
+      var rating= fields["Rating"];
+      var itemHTML = detailView(id, workname, picture, explanation, rating);
+      html.push(itemHTML);
+    html.push(`</div>`);
+    $(".detail-view").append(html.join(""));
+  });
+}
+
+// Do we have an ID in the URL?
+var id = getParameterByName("id");
+
+// If we have an ID, we should only get the data for one item
+// Otherwise, we should display the data for all items
+if (id) {
+  getDataForId(id);
+} else {
+  getDataForList();
+}
